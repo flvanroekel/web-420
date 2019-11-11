eb82bd54cfa3298085ed544de5221d2a5e4005ba
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-11-11 14:37:59 -0600

    assignment 4.4

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -3,19 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-var mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-
-
-mongoose.connect('mongodb+srv://admin:admin@buwebdev-cluster-1-2fw4y.mongodb.net:27017/api-gateway', {
-  promiseLibrary: require('bluebird')
-}).then(
-  () => console.log('connection successful'))
-.catch(
-  (err) => console.error(err));
-
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var apiCatalog = require('./routes/api-catalog');
@@ -23,18 +11,35 @@ var apiCatalog = require('./routes/api-catalog');
 
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+
+mongoose.connect('mongodb+srv://admin:admin@buwebdev-cluster-1-2fw4y.mongodb.net/api-gateway', {
+  promiseLibrary: require('bluebird')
+}).then(
+  () => console.log('connection successful'))
+.catch(
+  (err) => console.error(err));
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiCatalog);
+
+
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -12,48 +12,59 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
 
-
+//Register a new user when POSTed
 exports.user_register = function(req, res) {
-    
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);  //take plain text password sent from request form and hash it
 
-  var newUser = new User({
-      username: req.body.username,
-      password: hashedPassword,
-      email: req.body.email
+  var newUser = new User({ //get all request form controls and store in User object
+    username: req.body.username,
+    password: hashedPassword,
+    email: req.body.email
   });
 
   User.add(newUser, (err, user) => {
-      if (err) 
-          return res.status(500).send('There was a problem registering the user.');
+    if (err)
+      return res.status(500).send('There was a problem registering the user.');
 
-      var token = jwt.sign({ id: user._id}, config.web.secret, {
-          expiresIn: 86400 // 24 hours
-      });
+    var token = jwt.sign({ id: user._id }, config.web.secret, { //sign the user with the secret key stored in web configuration file
+      expiresIn: 86400
+    });
 
-      res.status(200).send({ auth: true, token: token });
+    res.status(200).send({ auth: true, token: token }); //send authenticated true along with the token??
   });
 };
 
-
+// Verify token on GET
 exports.user_token = function(req, res) { 
-  User.getById(req.userId, function(err, user) {
-      if (err) return res.status(500).send('There was a problem finding the user.');
+    User.getById(req.userId, function(err, user) {
+        if (err) return res.status(500).send('There was a problem finding the user.');
 
-      if (!user) return res.status(404).send('No user found.');
+        if (!user) return res.status(404).send('No user found.');
 
-      res.status(200).send(user);
-  });
+        res.status(200).send(user);
+    });
 };
 
+// Login as an existing user on POST
+exports.user_login = function(req, res) {
+    
+    User.getOne(req.body.email, function(err, user) {
+        if (err) return res.status(500).send('Error on server.');
+        if (!user) return res.status(404).send('No user found.');
 
-//Register a new user on POST
-exports.user_register = function(req, res) {
-    res.send('NOT IMPLEMENTED: User registration POST');
-  };
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
 
-  //verify token on GET
-exports.user_token = function(req, res) {
-    res.send('NOT IMPLEMENTED: User token lookup GET'); 
-  };
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null});
 
+        var token = jwt.sign({ id: user._id}, config.web.secret, {
+            expiresIn: 86400 // expires in 24 hours 
+        });
+
+        res.status(200).send( {auth: true, token: token });
+    })
+};
+
+// Logout an existing user
+exports.user_logout = function(req, res) {
+    res.status(200).send({ auth: false, token: null});
+}
